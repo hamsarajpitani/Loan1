c43e2e2c5fa1b32b04aae9aeb7ab13ffb5e59d0d
PitaniHamsaraj <33552374+hamsarajpitani@users.noreply.github.com>
2021-02-11 20:47:31 +0530

    Navbar close on click added

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
-var vue = new Vue({
-    el: '#app',
-    data: {
-      formOpen: false,
-      productData: {
-        title: '',
-        rating: '',
-        price: '',
-        list_price: '',
-        is_featured: false
-      }
-    },
-    methods: {
-      resetForm: function () {
-        this.productData = {
-          title: '',
-          rating: '',
-          price: '',
-          list_price: '',
-          is_featured: false
-        }
-      },
-      cancel: function() {
-        this.formOpen = false;
-        this.resetForm();
-      }
-    }
-  })
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
